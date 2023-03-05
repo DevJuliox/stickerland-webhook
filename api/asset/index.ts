d33489fb8749fa2635b6567bbde7566beee4c290
Jhorman Ruswel ㅠㅠ <jhormanrus@gmail.com>
2023-03-05 21:44:38 +0000

    cors

--- api/asset/index.ts
+++ api/asset/index.ts
@@ -3,7 +3,27 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 const api_key = process.env.CLOUDINARY_API_KEY
 const api_secret = process.env.CLOUDINARY_API_SECRET
 
-export default async function (req: VercelRequest, res: VercelResponse): Promise<VercelResponse> {
+const allowCors = fn => async (req, res) => {
+  const allowedOrigins = ['https://stickerland.vercel.app']
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', origin)
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+  }
+  res.status(403).end()
+}
+
+async function asset (req: VercelRequest, res: VercelResponse): Promise<VercelResponse> {
   if (req.method === 'GET') {
     const endpoint = 'https://api.cloudinary.com/v1_1/jhormanrus/resources/image/upload?prefix=stickerland'
     const headers = new Headers()
@@ -21,3 +41,5 @@ export default async function (req: VercelRequest, res: VercelResponse): Promise
   
   return res.status(400).end()
 }
+
+export default allowCors(asset)
